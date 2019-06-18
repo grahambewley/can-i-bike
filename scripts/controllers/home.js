a5eb92cdb03a50c4601d81fb7d2e04be0dcb4de7
Graham Bewley <grahambewley@gmail.com>
2019-06-17 23:24:56 -0400

    Begin linking up result to view

--- scripts/controllers/home.js
+++ scripts/controllers/home.js
@@ -3,6 +3,13 @@ canibike.controller('home', function($scope, $http) {
     $scope.latString = '';
     $scope.longString = '';
 
+    $scope.result = {
+        canI: '',
+        reasonCondition: '',
+        reasonTime: ''
+    };
+
+
     /*******DEFAULT THRESHOLDS*******/
 
     $scope.activityThresholds = [
@@ -82,24 +89,22 @@ canibike.controller('home', function($scope, $http) {
             "success": function(res){
                 //Take response string and parse into JSON object
                 var responseObject = JSON.parse(res);
+
                 //Send response to handler function
-                processResponse(responseObject);
+                processResponse(responseObject, $scope);
             }
         });
         
     }
 
-    function processResponse(responseObject) {
-       
+    function processResponse(responseObject, $scope) {
+
         //Remains false until we've looped through and hit our activity "start time" for the first time
         //Keeps us from hitting start time twice in the 48 hours of response data
         var hitStartHour = false;
         //Same thing for "end hour"...
         var hitEndHour = false;
 
-        var canI = true;
-        var reasonCondition = '';
-        var reasonTime = '';
 
         var hourlyArray = responseObject.hourly.data;
         console.log(hourlyArray);
@@ -107,82 +112,93 @@ canibike.controller('home', function($scope, $http) {
         console.log("Current activity start: " + $scope.selectedActivityTimes.startHour);
         console.log("Current activity end: " + $scope.selectedActivityTimes.endHour);
         
-        hourlyArray.forEach(element => {
-            //Make a new data object at epoch 0 -- 00:00 Jan 1, 1970
-            thisTimeObject = new Date(0);
-            //Add the epoch time (in seconds) that we get from the API
-            thisTimeObject.setUTCSeconds(element.time); 
-            
-            //If we have not yet hit our "start hour" and then we do...
-            if((hitStartHour == false) && (thisTimeObject.getHours() == $scope.selectedActivityTimes.startHour)) {
-                //set flag to true so we don't do this again...
-                hitStartHour = true;
+        $scope.$apply(function() {
+            hourlyArray.forEach(element => {
+                //Make a new data object at epoch 0 -- 00:00 Jan 1, 1970
+                thisTimeObject = new Date(0);
+                //Add the epoch time (in seconds) that we get from the API
+                thisTimeObject.setUTCSeconds(element.time); 
+                
+                //If we have not yet hit our "start hour" and then we do...
+                if((hitStartHour == false) && (thisTimeObject.getHours() == $scope.selectedActivityTimes.startHour)) {
+                    //set flag to true so we don't do this again...
+                    hitStartHour = true;
 
-                //Check start conditions against high temperature threshold
-                if(element.temperature > $scope.selectedActivityThresholds.highTempThreshold) {
-                    canI = false;
-                    reasonCondition = 'tempHigh';
-                    reasonTime = 'start';
-                    console.log("CANT! " + reasonCondition + " at " + reasonTime);
-                } 
-                //Check start conditions against low temperature threshold
-                else if(element.temperature < $scope.selectedActivityThresholds.lowTempThreshold) {
-                    canI = false;
-                    reasonCondition = 'tempLow';
-                    reasonTime = 'start';
-                    console.log("CANT! " + reasonCondition + " at " + reasonTime);
+                    //Check start conditions against high temperature threshold
+                    if(element.temperature > $scope.selectedActivityThresholds.highTempThreshold) {
+                        $scope.result.canI = 'No';
+                        $scope.result.reasonCondition = 'tempHigh';
+                        $scope.result.reasonTime = 'start';
+                        console.log("CANT! " + $scope.result.reasonCondition + " at " + $scope.result.reasonTime);
+                    } 
+                    //Check start conditions against low temperature threshold
+                    else if(element.temperature < $scope.selectedActivityThresholds.lowTempThreshold) {
+                        $scope.result.canI = 'No';
+                        $scope.result.reasonCondition = 'tempLow';
+                        $scope.result.reasonTime = 'start';
+                        console.log("CANT! " + $scope.result.reasonCondition + " at " + $scope.result.reasonTime);
+                    }
+
+                    if(element.windSpeed > $scope.selectedActivityThresholds.windSpeedThreshold) {
+                        $scope.result.canI = 'No';
+                        $scope.result.reasonCondition = 'windSpeed';
+                        $scope.result.reasonTime = 'start';
+                        console.log("CANT! " + $scope.result.reasonCondition + " at " + $scope.result.reasonTime);
+                    }
+
+                    if(element.precipProbability > $scope.selectedActivityThresholds.precipProbThreshold) {
+                        $scope.result.canI = 'No';
+                        $scope.result.reasonCondition = 'precipProb';
+                        $scope.result.reasonTime = 'start';
+                        console.log("CANT! " + $scope.result.reasonCondition + " at " + $scope.result.reasonTime);
+                    }
+
+                };
+
+                //If we have not yet hit our "end hour" and then we do...
+                if((hitEndHour == false) && (thisTimeObject.getHours() == $scope.selectedActivityTimes.endHour)) {
+                    //set flag to true so we don't do this again...
+                    hitEndHour = true;
+
+                    //Check end conditions against high temperature threshold
+                    if(element.temperature > $scope.selectedActivityThresholds.highTempThreshold) {
+                        $scope.result.canI = 'No';
+                        $scope.result.reasonCondition = 'tempHigh';
+                        $scope.result.reasonTime = 'end';
+                        console.log("CANT! " + $scope.result.reasonCondition + " at " + $scope.result.reasonTime);
+                    } 
+                    //Check end conditions against low temperature threshold
+                    else if(element.temperature < $scope.selectedActivityThresholds.lowTempThreshold) {
+                        $scope.result.canI = 'No';
+                        $scope.result.reasonCondition = 'tempLow';
+                        $scope.result.reasonTime = 'end';
+                        console.log("CANT! " + $scope.result.reasonCondition + " at " + $scope.result.reasonTime);
+                    }
+
+                    if(element.windSpeed > $scope.selectedActivityThresholds.windSpeedThreshold) {
+                        $scope.result.canI = 'No';
+                        $scope.result.reasonCondition = 'windSpeed';
+                        $scope.result.reasonTime = 'end';
+                        console.log("CANT! " + $scope.result.reasonCondition + " at " + $scope.result.reasonTime);
+                    }
+
+                    if(element.precipProbability > $scope.selectedActivityThresholds.precipProbThreshold) {
+                        $scope.result.canI = 'No';
+                        $scope.result.reasonCondition = 'precipProb';
+                        $scope.result.reasonTime = 'end';
+                        console.log("CANT! " + $scope.result.reasonCondition + " at " + $scope.result.reasonTime);
+                    }
+
                 }
-
-                if(element.windSpeed > $scope.selectedActivityThresholds.windSpeedThreshold) {
-                    canI = false;
-                    reasonCondition = 'windSpeed';
-                    reasonTime = 'start';
-                    console.log("CANT! " + reasonCondition + " at " + reasonTime);
-                }
-
-                if(element.precipProbability > $scope.selectedActivityThresholds.precipProbThreshold) {
-                    canI = false;
-                    reasonCondition = 'precipProb';
-                    reasonTime = 'start';
-                    console.log("CANT! " + reasonCondition + " at " + reasonTime);
-                }
-
-            };
-
-            //If we have not yet hit our "start hour" and then we do...
-            if((hitEndHour == false) && (thisTimeObject.getHours() == $scope.selectedActivityTimes.endHour)) {
-                //set flag to true so we don't do this again...
-                hitEndHour = true;
-
-                //Check end conditions against high temperature threshold
-                if(element.temperature > $scope.selectedActivityThresholds.highTempThreshold) {
-                    canI = false;
-                    reasonCondition = 'tempHigh';
-                    reasonTime = 'end';
-                    console.log("CANT! " + reasonCondition + " at " + reasonTime);
-                } 
-                //Check end conditions against low temperature threshold
-                else if(element.temperature < $scope.selectedActivityThresholds.lowTempThreshold) {
-                    canI = false;
-                    reasonCondition = 'tempLow';
-                    reasonTime = 'end';
-                    console.log("CANT! " + reasonCondition + " at " + reasonTime);
-                }
-
-                if(element.windSpeed > $scope.selectedActivityThresholds.windSpeedThreshold) {
-                    canI = false;
-                    reasonCondition = 'windSpeed';
-                    reasonTime = 'end';
-                    console.log("CANT! " + reasonCondition + " at " + reasonTime);
-                }
-
-                if(element.precipProbability > $scope.selectedActivityThresholds.precipProbThreshold) {
-                    canI = false;
-                    reasonCondition = 'precipProb';
-                    reasonTime = 'end';
-                    console.log("CANT! " + reasonCondition + " at " + reasonTime);
-                }
-
+            });
+        });
+        
+        $scope.$apply(function() {
+            if($scope.result.canI == '') {
+                console.log("Setting result.canI to YES");
+                $scope.result.canI = 'Yes';
+            } else {
+                $scope.result.canI = 'No';
             }
         });
     }
